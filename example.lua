f3b45a8b07bc3b2ed18b4d1e45f596255537c321
Christopher Birmingham <chris.birmingham@hotmail.co.uk>
2017-06-21 18:40:11 +0000

    Started userdata coding
    Started converting the script to user luas userdata type so that many instances
    of the "object" can be used at the same time

--- example.lua
+++ example.lua
@@ -1,27 +1,38 @@
-package.cpath = package.cpath .. ";./lib/?.so"
-local taglib = require "taglib"
+local songReader = require ("taglib.tagReader")
 
 --[[
   This file shows what TagLib functions are supported by lua-taglib
   and how to use them.
-  ]]--
+]]
 
-success = taglib.file('music.mp3')
-if success then
+-- File exists
+song = songReader.new('song.mp3')
 
-    -- tags
-    tags = taglib.tag()
-    print("Artist: " .. tags.artist)
-    print("Album: " .. tags.album)
-    print("Title: " .. tags.title)
-    print("Genre: " .. tags.genre)
-    print("Track: " .. tags.track)
-    print("Year: " .. tags.year)
-    print("Comment: " .. tags.comment)
+tags = songReader.tags(song)
+print("Artist: " .. tags.artist)
+print("Album: " .. tags.album)
+print("Title: " .. tags.title)
+print("Genre: " .. tags.genre)
+print("Track: " .. tags.track)
+print("Year: " .. tags.year)
+print("Comment: " .. tags.comment)
 
-    -- audio values
-    print("Length: " .. taglib.length())
-    print("Bitrate: " .. taglib.bitrate())
-    print("Sample Rate: " .. taglib.sampleRate())
-    print("Channels: " .. taglib.channels())
+-- audio values
+print("Length: " .. songReader.length(song))
+--print("Bitrate: " .. taglib.bitrate())
+--print("Sample Rate: " .. taglib.sampleRate())
+--print("Channels: " .. taglib.channels())
+--
+
+songReader.free(song)
+
+-- File does not exist
+song = songReader.new('fake.mp3')
+
+tags = songReader.tags(song)
+
+if tags == nil then
+    print('File does not exist')
 end
+
+songReader.free(song)
--- src/tagReader.cpp
+++ src/tagReader.cpp
@@ -1,65 +1,114 @@
-#include "lua.hpp"
+#include <lua.hpp>
 
 #include <taglib/fileref.h>
 #include <taglib/tag.h>
 
-TagLib::FileRef f;
+#define TAG_READER "TagReader"
 
-static int file(lua_State* L)
+typedef struct tagReader {
+    TagLib::FileRef* f;
+} TagReader;
+
+/**
+ * Checks whether the first argument to a function of the userdata tag reader
+ */
+static TagReader* checkReader(lua_State* L)
 {
-    const char *in = luaL_checkstring(L, 1);
-    bool success = false;
-    f = TagLib::FileRef(in);
-    if(!f.isNull()) {
-        success = true;
-    }
-    lua_pushboolean(L, success);
+    void* reader = luaL_checkudata(L, 1, TAG_READER);
+    luaL_argcheck(L, reader != NULL, 1, "`tag reader' expected");
+    return (TagReader*)reader;
+}
+
+/**
+ * Creates a tag reader userdata
+ */
+static TagReader* createReader(lua_State* L)
+{
+    TagReader* reader = (TagReader*)lua_newuserdata(L, sizeof(TagReader) + sizeof(TagLib::FileRef*));
+    luaL_getmetatable(L, TAG_READER);
+    lua_setmetatable(L, -2);
+    return reader;
+}
+
+/**
+ * Creates a tag reader userdata and associate a tagfile with it
+ */
+static int new_tagReader(lua_State* L)
+{
+    const char* file = luaL_checkstring(L, 1);
+    TagReader* reader = createReader(L);
+    reader->f = new TagLib::FileRef(file);
     return 1;
 }
 
-static int tag(lua_State *L)
+/**
+ * Retrives a song file metadata tags
+ */
+static int tags(lua_State *L)
 {
-    lua_newtable(L);
-    if(!f.isNull() && f.tag()) {
+    TagReader* reader = checkReader(L);
+    TagLib::FileRef* f = reader->f;
+    if(!f->isNull() && f->tag()) {
+        lua_newtable(L);
         // ARTIST
         lua_pushstring(L, "artist");
-        lua_pushstring(L, f.tag()->artist().toCString());
+        lua_pushstring(L, f->tag()->artist().toCString());
         lua_rawset(L, -3);
         // ALBUM
         lua_pushstring(L, "album");
-        lua_pushstring(L, f.tag()->album().toCString());
+        lua_pushstring(L, f->tag()->album().toCString());
         lua_rawset(L, -3);
         // TITLE
         lua_pushstring(L, "title");
-        lua_pushstring(L, f.tag()->title().toCString());
+        lua_pushstring(L, f->tag()->title().toCString());
         lua_rawset(L, -3);
         // GENRE
         lua_pushstring(L, "genre");
-        lua_pushstring(L, f.tag()->genre().toCString());
+        lua_pushstring(L, f->tag()->genre().toCString());
         lua_rawset(L, -3);
         // TRACKNUMBER
         lua_pushstring(L, "track");
-        lua_pushinteger(L, f.tag()->track());
+        lua_pushinteger(L, f->tag()->track());
         lua_rawset(L, -3);
         // DATE
         lua_pushstring(L, "year");
-        lua_pushinteger(L, f.tag()->year());
+        lua_pushinteger(L, f->tag()->year());
         lua_rawset(L, -3);
         // COMMENT
         lua_pushstring(L, "comment");
-        lua_pushstring(L, f.tag()->comment().toCString(true));
+        lua_pushstring(L, f->tag()->comment().toCString(true));
         lua_rawset(L, -3);
+    } else {
+        lua_pushnil(L);
     }
     return 1;
 }
 
-static int length(lua_State *L)
+/**
+ * Retrive the length of the song
+ */
+static int length(lua_State* L)
 {
-    lua_pushinteger(L, f.audioProperties()->length());
+    TagReader* reader = checkReader(L);
+    TagLib::FileRef* f = reader->f;
+    if (!f->isNull()) {
+        lua_pushinteger(L, f->audioProperties()->length());
+    } else {
+        lua_pushnil(L);
+    }
     return 1;
 }
 
-static int bitrate(lua_State *L)
+/**
+ * Frees the file object stored in the userdata
+ */
+static int free_tagReader(lua_State* L)
+{
+    TagReader* reader = checkReader(L);
+    free(reader->f);
+}
+
+/**static int bitrate(lua_State *L)
 {
     lua_pushinteger(L, f.audioProperties()->bitrate());
     return 1;
@@ -74,25 +123,28 @@ static int channels(lua_State *L)
 {
     lua_pushinteger(L, f.audioProperties()->channels());
     return 1;
-}
+}*/
 
-static const luaL_Reg API[] = {
-    {"file", file },
-    { "tag", tag },
-    { "length", length },
-    { "bitrate", bitrate },
-    { "sampleRate", sampleRate },
-    { "channels", channels },
+/** Define the function names for lua to call */
+static const luaL_Reg tag_reader_f[] = {
+    {"tags", tags},
+    {"length", length},
+    {"new", new_tagReader},
+    {"free", free_tagReader},
     { NULL, NULL },
+};
+
+/** Define the lua mappings for the userdata */
+static const luaL_Reg tag_reader_m[] = {
+    {"tags", tags},
+    {"length", length},
+    {NULL, NULL}    
 };
 
 LUALIB_API "C" int luaopen_taglib_tagReader(lua_State* L)
 {
+    luaL_newmetatable(L, TAG_READER);
     lua_newtable(L);
-#if LUA_VERSION_NUM < 502
-    luaL_register(L, NULL , API);
-#else 
-    luaL_setfuncs(L, API, 0);
-#endif
+    luaL_setfuncs(L, tag_reader_f, 0);
     return 1;
 }
